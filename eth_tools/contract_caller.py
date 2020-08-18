810d637808555ffcf6f71da501679283fc526a5f
Daniel Perez <daniel@perez.sh>
2020-08-18 20:05:38 +0100

    Fix progress and add error handling to contract caller

--- eth_tools/contract_caller.py
+++ eth_tools/contract_caller.py
@@ -2,6 +2,7 @@ from concurrent.futures import ThreadPoolExecutor
 import multiprocessing
 
 from web3.contract import Contract
+from retry import retry
 
 from eth_tools.logger import logger
 
@@ -17,17 +18,24 @@ class ContractCaller:
         max_workers = multiprocessing.cpu_count() * 5
         if end_block is None:
             end_block = self.contract.web3.eth.blockNumber
-        total_count = end_block - start_block + 1
+
         def run_task(block):
-            return self.call_func(func_name, block, *args, **kwargs)
+            try:
+                return self.call_func(func_name, block, *args, **kwargs)
+            except Exception as ex: # pylint: disable=broad-except
+                logger.error("failed to fetch block %s: %s", block, ex)
+
         with ThreadPoolExecutor(max_workers=max_workers) as executor:
             blocks = range(start_block, end_block + 1, block_interval)
+            total_count = len(blocks)
             results = executor.map(run_task, blocks)
             for i, (block, result) in enumerate(zip(blocks, results)):
                 if i % 10 == 0:
-                    logger.info("progress: %s/%s", i, total_count)
-                yield (block, result)
+                    logger.info("progress: %s/%s (%.2f%%)", i, total_count, i / total_count)
+                if result:
+                    yield (block, result)
 
+    @retry(delay=1, backoff=2, tries=3, logger=logger)
     def call_func(self, func_name, block, *args, **kwargs):
         func = getattr(self.contract.functions, func_name)
         return func(*args, **kwargs).call(block_identifier=block)
